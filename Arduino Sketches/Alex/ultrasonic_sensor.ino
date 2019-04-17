8f6389c16c5719bff93b2c82497bbbd94b0d05c4
hanskw4267 <hanskurniaw@gmail.com>
2019-04-17 14:09:57 +0800

    revert ultrasonic clean-up

--- Arduino Sketches/Alex/ultrasonic_sensor.ino
+++ Arduino Sketches/Alex/ultrasonic_sensor.ino
@@ -1,13 +1,14 @@
 int trigPin = 13;    // Trigger
 int echoPinC = 12;    // Echo
+int echoPinL = 7;
+int echoPinR = 9;
 long duration, cm;
-
-/*
-  file contains ultrasonic function
-*/
+ 
 void setupUSensor() {
   pinMode(trigPin, OUTPUT);
+  pinMode(echoPinL, INPUT);
  pinMode(echoPinC, INPUT);
+ pinMode(echoPinR, INPUT);
 }
  
 long loopUSensor() {
@@ -17,7 +18,23 @@ long loopUSensor() {
   delayMicroseconds(10);
   digitalWrite(trigPin, LOW);
   
+  if(dir == FORWARD || dir == STOP || dir == BACKWARD)
+  {
   duration = pulseIn(echoPinC, HIGH);
   cm = (duration/2) / 29.1;
   ultra_dist_C = cm;
+  }
+  else if(dir == RIGHT)
+  {
+  duration = pulseIn(echoPinR, HIGH);
+  cm = (duration/2) / 29.1;
+  ultra_dist_R = cm;
+  }
+  else if(dir == LEFT)
+  {
+  duration = pulseIn(echoPinL, HIGH);
+  cm = (duration/2) / 29.1;
+  ultra_dist_L = cm;
+  }
+
 }
